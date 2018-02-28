1b27fbb2493cced481915de99af2f205a8672961
shooter46 <36916585+shooter46@users.noreply.github.com>
2018-02-28 14:13:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function createArg(type, name, content) {
+  var element = '<div class="arg-block">';
+  element += '<span class="arg-type">'+type+"</span> "+name;
+
+  if (content) {
+    element += '<div class="arg-desc">'+content+"</div></div>";
+  }
+
+  return element;
+}
+
 module.exports = {
 
   book: {
@@ -6,14 +17,25 @@ module.exports = {
       'shpl4r.css'
     ]
   },
+
+  blocks: {
+    Arg: {
+      process: function(block) {
+        var type = block.kwargs.type || 'default';
+        var name = block.kwargs.name;
+
+        return this
+            .renderBlock('markdown', block.body)
+            .then(function(renderedBody) {
+              return createArg(type, name, renderedBody);
+            });
+      }
+    }
+  },
   filters: {
-	Arg: function(type, name, desc) {
-	  var str = '<div class="arg-block">';
-	  str += '<span class="arg-type">'+type+"</span> "+name;
-	  if (content) {
-		str += '<div class="arg-desc">'+desc+"</div></div>";
-	  }
-	  return name;
-	}
+	warning: function(name) {
+      return 'Hello '+name;
+    }
   }
+
 };
