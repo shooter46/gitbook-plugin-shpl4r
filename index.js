4ca4880a3b8086fb48b55f5010dfa8960b1c70f8
shooter46 <36916585+shooter46@users.noreply.github.com>
2018-02-28 12:53:37 +0300

    1
    1

---
+++ index.js
@@ -0,0 +1,36 @@
+function createArg(type, name, content) {
+  var element = '<div class="arg-block">';
+  element += '<span class="arg-type">'+type+"</span> "+name;
+
+  if (content) {
+    element += '<div class="arg-desc">'+content+"</div></div>";
+  }
+
+  return element;
+}
+
+module.exports = {
+
+  book: {
+    assets: './assets',
+    css: [
+      'shpl4r.css'
+    ]
+  },
+
+  blocks: {
+    Arg: {
+      process: function(block) {
+        var type = block.kwargs.type || 'default';
+        var name = block.kwargs.name;
+
+        return this
+            .renderBlock('markdown', block.body)
+            .then(function(renderedBody) {
+              return createArg(type, name, renderedBody);
+            });
+      }
+    }
+  }
+
+};
